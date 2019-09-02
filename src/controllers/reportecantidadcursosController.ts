80ac4bbb31a0c0c6b50bc2b2c7724db8981aba87
HancelGH <54192092+HancelGH@users.noreply.github.com>
2019-09-02 16:40:29 -0600

    controlador reporte 3

---
+++ src/controllers/reportecantidadcursosController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import pool from '../db';
+
+class reportecantidadcController{
+
+    public async getAllreporte (req:Request, res:Response) {
+        const rows = await pool.query('Call reporte3()');
+        res.json(rows[0]);
+    }
+
+}
+
+const reporte3Controller = new reportecantidadcController();
+export default reporte3Controller;
