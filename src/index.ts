b97d70350f7761cbbcd7c3401a0ec519ee5afcfe
Juan Guerra <jguerral4@miumg.edu.gt>
2019-09-04 15:03:24 -0600

    Rutas de login

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import reporte2Routes from './routes/reportecatedraticosRoutes';
 import path from 'path';
+import loginRoutes from './routes/loginRoutes';
 
 class Server{
 
@@ -49,6 +50,7 @@ class Server{
         this.app.use('/cruds/asignaciones', asigRoutes);
         this.app.use('/reportes/reporte3',reporte3Routes);
         this.app.use('/reportes/reporte2',reporte2Routes);
+        this.app.use('/login',loginRoutes);
     }
 
     iniciar():void{
