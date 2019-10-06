b05143ac25a1894eddc09501e845ac88ce6327e1
Juan Guerra <jguerral4@miumg.edu.gt>
2019-10-06 15:38:37 -0600

    Se deja la base de datos en localhost para desarrollo

--- src/config.db.ts
+++ src/config.db.ts
@@ -1,8 +1,8 @@
 export default {
     database: {
-        //host: 'localhost',
+        host: 'localhost',
         //host: 'mysqldev.cwveojqnk2sk.us-east-1.rds.amazonaws.com',
-        host:'mysqlumg.cwveojqnk2sk.us-east-1.rds.amazonaws.com',
+        //host:'mysqlumg.cwveojqnk2sk.us-east-1.rds.amazonaws.com',
         user: 'usuario',
         password: 'usuario',
         database: 'db_universidad',
