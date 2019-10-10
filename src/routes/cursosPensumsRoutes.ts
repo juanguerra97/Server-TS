67eb109eefec17ecec1d804a06a981aa61388eae
Juan Guerra <jguerral4@miumg.edu.gt>
2019-10-10 11:56:34 -0600

    Manejo de excepciones y formato estandar de respuestas para los endpoint de cursospensums

--- src/routes/cursosPensumsRoutes.ts
+++ src/routes/cursosPensumsRoutes.ts
@@ -12,9 +12,11 @@ class CursosPensumsRoutes{
 
     config():void{
         this.router.get('/',cursosPensumsController.getAllCursosByPensumAndCiclo);
-        this.router.get('/:za_carrera/:ano_pensum', cursosPensumsController.getAllCursosPensums);
-        this.router.get('/:za_carrera/:ano_pensum/:za_curso', cursosPensumsController.getUnoCursosPensums);
-        this.router.post('/', cursosPensumsController.opcionesCursosPensums);
+        this.router.get('/:za_carrera(\\d+)/:ano_pensum(\\d+)', cursosPensumsController.getAllCursosPensums);
+        this.router.get('/:za_carrera(\\d+)/:ano_pensum(\\d+)/:za_curso(\\d+)', cursosPensumsController.getUnoCursosPensums);
+        this.router.post('/', cursosPensumsController.insert);
+        this.router.delete('/:za_carrera(\\d+)/:ano_pensum(\\d+)/:za_curso(\\d+)',cursosPensumsController.delete);
+        this.router.put('/:za_carrera(\\d+)/:ano_pensum(\\d+)/:za_curso(\\d+)',cursosPensumsController.update);
     }
 
 }
